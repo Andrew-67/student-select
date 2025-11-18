32f17119b55131499993a1fdcabbbaac453d33b9
Reece, Andrew <andrew.reece@student.swic.edu>
2025-11-17 19:36:24 -0600

    Created Data

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import "./style.css";
 
+const student_data = [
+  {
+    id: 0,
+    name: "Andrew"
+  },
+  {
+    id: 1,
+    name: "John"
+  },
+  {
+    id: 2,
+    name: "Alice"
+  }
+];
+
 document.querySelector("#app").innerHTML = `
   <h1 class="text-3xl font-bold underline">
     Hello Vite!
