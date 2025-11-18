063ea38711e4155d61b7698d102b34354c4c02fc
Reece, Andrew <andrew.reece@student.swic.edu>
2025-11-17 19:53:00 -0600

    Fixed Reset Button

--- src/main.js
+++ src/main.js
@@ -21,6 +21,7 @@ document.querySelector("#app").innerHTML = `
     Hello Vite!
   </h1>
 `;
+
 const button = document.createElement("button");
 button.textContent = "Pick a Random Student";
 button.className =
@@ -32,9 +33,20 @@ button.addEventListener("click", () => {
   const messageContainer = document.getElementById("message-container");
   messageContainer.innerHTML = messageHTML;
 });
-document.body.appendChild(button);
 
 const messageContainer = document.createElement("div");
 messageContainer.id = "message-container";
 messageContainer.className = "mt-4";
+
+const resetbutton = document.createElement("button");
+resetbutton.textContent = "Reset";
+resetbutton.className =
+  "ml-4 px-4 py-2 bg-red-500 text-white font-semibold rounded hover:bg-red-600";
+resetbutton.addEventListener("click", () => {
+  const messageContainer = document.getElementById("message-container");
+  messageContainer.innerHTML = "";
+});
+
+document.body.appendChild(button);
+document.body.appendChild(resetbutton);
 document.body.appendChild(messageContainer);
