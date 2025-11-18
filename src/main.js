da38e150ec9364f9eff60b4063b08187b724f7f6
Reece, Andrew <andrew.reece@student.swic.edu>
2025-11-17 20:45:27 -0600

    Light and Dark Mode

--- src/main.js
+++ src/main.js
@@ -42,7 +42,22 @@ resetbutton.addEventListener("click", () => {
   const messageContainer = document.getElementById("message-container");
   messageContainer.innerHTML = "";
 });
+const lmb = document.createElement("button")
+lmb.textContent = "Light Mode"
+lmb.className ="mt-4 px-4 py-2 bg-gray-500 text-white font-semibold rounded hover:bg-gray-600"
+lmb.onclick = () => {
+  document.body.style.backgroundColor = "white";
+}
+
+const dmb = document.createElement("button")
+dmb.textContent = "Dark Mode"
+dmb.className ="mt-4 px-4 py-2 bg-gray-500 text-white font-semibold rounded hover:bg-gray-600"
+dmb.onclick = () => {
+  document.body.style.backgroundColor = "gray";
+}
 
 document.body.appendChild(button);
 document.body.appendChild(resetbutton);
+document.body.appendChild(lmb);
+document.body.appendChild(dmb);
 document.body.appendChild(messageContainer);
